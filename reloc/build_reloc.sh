5c49c9794d4d9a33df1f61f9a2ec5caa95f59f5e
Takuya ASADA <syuu@scylladb.com>
2019-12-23 16:46:46 +0900

    inital import

---
+++ reloc/build_reloc.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+#
+# Copyright (C) 2019 ScyllaDB
+#
+
+#
+# This file is part of Scylla.
+#
+# Scylla is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Scylla is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+TARGET=build/scylla-python3-package.tar.gz
+
+if [ -f "$TARGET" ]; then
+    rm "$TARGET"
+fi
+
+./SCYLLA-VERSION-GEN
+mkdir -p build/python3
+./dist/debian/debian_files_gen.py
+
+PACKAGES="python3-pyyaml python3-urwid python3-pyparsing python3-requests python3-pyudev python3-setuptools python3-psutil python3-distro"
+./scripts/create-relocatable-package.py --output "$TARGET" $PACKAGES
---
+++ reloc/build_rpm.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -e
+
+. /etc/os-release
+print_usage() {
+    echo "build_rpm.sh --reloc-pkg build/scylla-python3-package.tar.gz"
+    echo "  --reloc-pkg specify relocatable package path"
+    echo "  --builddir specify rpmbuild directory"
+    exit 1
+}
+RELOC_PKG=build/scylla-python3-package.tar.gz
+BUILDDIR=build/redhat
+OPTS=""
+while [ $# -gt 0 ]; do
+    case "$1" in
+        "--reloc-pkg")
+            OPTS="$OPTS $1 $(readlink -f $2)"
+            RELOC_PKG=$2
+            shift 2
+            ;;
+        "--builddir")
+            BUILDDIR="$2"
+            shift 2
+            ;;
+        *)
+            print_usage
+            ;;
+    esac
+done
+
+if [ ! -e $RELOC_PKG ]; then
+    echo "$RELOC_PKG does not exist."
+    echo "Run ./reloc/build_reloc.sh first."
+    exit 1
+fi
+RELOC_PKG=$(readlink -f $RELOC_PKG)
+if [[ ! $OPTS =~ --reloc-pkg ]]; then
+    OPTS="$OPTS --reloc-pkg $RELOC_PKG"
+fi
+mkdir -p $BUILDDIR/scylla-python3
+tar -C $BUILDDIR -xpf $RELOC_PKG scylla-python3/SCYLLA-RELOCATABLE-FILE scylla-python3/SCYLLA-RELEASE-FILE scylla-python3/SCYLLA-VERSION-FILE scylla-python3/SCYLLA-PRODUCT-FILE scylla-python3/dist/redhat
+cd $BUILDDIR/scylla-python3
+exec ./dist/redhat/build_rpm.sh $OPTS
